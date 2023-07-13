c2c716cd1a548614beb555ac7ed16e539736225d
Ann Likowo Molinge <ann.molinge@gmail.com>
2023-07-13 17:07:57 -0500

    Counter ReactApp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'; //use to update and track data wheen needed
+
 
 function App() {
+  //initializing the useState
+  const [count, setCount] = useState(0)
+  const [evenCount, setEvenCount] = useState(0)
+  const[color, setColor] = useState('even')
+  //count is the variable that tells us what will return once the fxn runs while setCount updates the data in the counter
+  console.log(count)
+// now create any fxns needed. Fxns always come before the return portion
+  const addOne = () => {
+    setCount(count +1)
+    if((count +1) %2 === 0){
+       setEvenCount(evenCount +1)
+       setColor('even')
+    }else {
+      setColor('odd')
+    }
+  }
+
+  const minusOne = () => {
+    setCount(count -1)
+    if((count +1) %2 === 0){
+      setEvenCount(evenCount +1)
+      setColor('even')
+   }else{
+    setColor('odd')
+   }
+   
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter</h1>
+      <h3> Single Count: <p id={color}> {count} </p> 
+
+       </h3>
+      <button onClick={addOne} > Add One </button>
+      <button onClick={minusOne} > Minus One </button>
+
+      <h3> Even Count :{evenCount} </h3>
     </div>
   );
 }
 
 export default App;
+
+//
+{/* <h3> Single Count: {count %2 === 0 ? <p id='even' > {count} </p>: <p id='odd' > {count} </p>} 
+</h3> */}
